42cf9cba739874e2e61e6f3fa7a79ba91c24fb27
Javier Flores <81644684+javierIA@users.noreply.github.com>
2022-12-16 17:50:07 -0700

    op

---
+++ CNN/main.py
@@ -0,0 +1,13 @@
+import cv2 
+
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    #print(frame)
+    print(frame.shape)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
